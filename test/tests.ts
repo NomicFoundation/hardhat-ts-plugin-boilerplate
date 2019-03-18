aad6dfa3df9d5f05078951638ad07f02dfd80b50
Patricio Palladino <email@patriciopalladino.com>
2019-03-18 10:26:36 -0300

    Fix the tests. They broke with Buidler beta 2

--- test/tests.ts
+++ test/tests.ts
@@ -1,8 +1,6 @@
 import { BuidlerRuntimeEnvironment } from "@nomiclabs/buidler/types";
 import { assert } from "chai";
 
-import { ExampleBuidlerRuntimeEnvironmentField } from "../src/index";
-
 declare module "mocha" {
   interface Context {
     env: BuidlerRuntimeEnvironment;
@@ -10,18 +8,28 @@ declare module "mocha" {
 }
 
 describe("BuidlerRuntimeEnvironment extension", function() {
-  beforeEach("Buidler project setup", function() {
+  let exampleBuidlerRuntimeEnvironmentFieldClass: any;
+
+  before("Buidler project setup", async function() {
     process.chdir(__dirname + "/buidler-project");
     process.env.BUIDLER_NETWORK = "develop";
 
-    // We first clear any cache
-    delete require.cache[require.resolve("@nomiclabs/buidler")];
-
     this.env = require("@nomiclabs/buidler");
+
+    // The plugins' index must not be imported before the lib,
+    // as it needs Buidler to be initialized.
+    const {
+      ExampleBuidlerRuntimeEnvironmentField
+    } = await import("../src/index");
+
+    exampleBuidlerRuntimeEnvironmentFieldClass = ExampleBuidlerRuntimeEnvironmentField;
   });
 
-  it("It should add the example field", function() {
-    assert.instanceOf(this.env.example, ExampleBuidlerRuntimeEnvironmentField);
+  it("It should add the example field", async function() {
+    assert.instanceOf(
+      this.env.example,
+      exampleBuidlerRuntimeEnvironmentFieldClass
+    );
   });
 
   it("The example filed should say hello", function() {
